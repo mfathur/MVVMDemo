5dbbf40cddb4405b9ce90127d7cfbbaca43d8987
mfathur <mfathur17@gmail.com>
2021-09-09 13:04:42 +0700

    fix offline first code

--- app/src/main/java/com/omahti/mvvmdemo/home/HomeViewModel.kt
+++ app/src/main/java/com/omahti/mvvmdemo/home/HomeViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.omahti.mvvmdemo.core.DataMapper
 import com.omahti.mvvmdemo.data.Repository
 import com.omahti.mvvmdemo.data.source.local.PostEntity
 import kotlinx.coroutines.launch
@@ -18,10 +19,14 @@ class HomeViewModel(private val repository: Repository) : ViewModel() {
 
     fun getAllPosts() = viewModelScope.launch {
         try {
+            _posts.postValue(repository.getAllPostsFromDb())
             val response = repository.getAllPostsFromNetwork()
+
             if (response.isSuccessful) {
-                response.body()?.let { repository.insertAllPostsToDb(it) }
-                _posts.postValue(repository.getAllPostsFromDb())
+                response.body()?.let {
+                    repository.insertAllPostsToDb(it)
+                    _posts.postValue(DataMapper.mapPostResponsesToEntities(it))
+                }
             }
         } catch (e: Exception) {
 
